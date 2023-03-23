224bb0feb9dd3a6bed6d13ecdc7c3c225a2e3da6
Douglas Santos <dzintv@yahoo.com>
2023-03-22 20:03:38 -0400

    Uso dos metodos get e set

--- app/src/main/java/devandroid/silva/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/silva/applistacurso/view/MainActivity.java
@@ -12,8 +12,10 @@ public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
     Pessoa outraPessoa;
-
     Construcao construcao;
+
+    String dadosPessoa;
+    String dadosOutraPessoa;
 
 
     @Override
@@ -29,17 +31,35 @@ public class MainActivity extends AppCompatActivity {
         pessoa.setCursoDesejado("Android");
         pessoa.setTelefoneContato("11-979565411");
 
-         outraPessoa = new Pessoa();
-         outraPessoa.setPrimeiroNome("Nadabe");
-         outraPessoa.setSobreNome("Maximo da Silva");
-         outraPessoa.setCursoDesejado("Java");
-         outraPessoa.setTelefoneContato("11-15615615.");
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Nadabe");
+        outraPessoa.setSobreNome("Maximo da Silva");
+        outraPessoa.setCursoDesejado("Java");
+        outraPessoa.setTelefoneContato("11-15615615.");
 
-         construcao = new Construcao();
-         construcao.setTipoDeTrabalho("Drywall");
-         construcao.setTamanhoDotrabalho("Grande");
-         construcao.setTelefoneContato("8628462889");
+        construcao = new Construcao();
+        construcao.setTipoDeTrabalho("Drywall");
+        construcao.setTamanhoDotrabalho("Grande");
+        construcao.setTelefoneContato("8628462889");
 
+        dadosPessoa = "Primeiro nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de Contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+
+        dadosOutraPessoa = "Primeiro nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobreNome();
+        dadosOutraPessoa += " Curso desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone de Contato: ";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
 
         int parada = 0;
 

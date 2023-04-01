ff9dce3a2151c6b0e7893f43ff2a458d098474a8
Douglas Santos <dzintv@yahoo.com>
2023-03-31 22:53:45 -0400

    Aplicando o metodo de salvar no Controller

--- app/src/main/java/devandroid/silva/applistacurso/controller/PessoaController.java
+++ app/src/main/java/devandroid/silva/applistacurso/controller/PessoaController.java
@@ -1,4 +1,24 @@
 package devandroid.silva.applistacurso.controller;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import devandroid.silva.applistacurso.model.Pessoa;
+
 public class PessoaController {
+
+    @NonNull
+    @Override
+    public String toString() {
+
+        return super.toString();
+    }
+    public void salvar(Pessoa pessoa){
+        Log.d("MVC_Controller", "Salvo: "+pessoa.toString());
+
+    }
+
 }
+
+
